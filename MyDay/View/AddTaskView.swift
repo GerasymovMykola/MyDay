8ff2d15904e79badbd5c0789ac6457565bf93da9
MH <gerasymovmykola@gmail.com>
2024-12-18 20:23:06 -0400

    new vercion app

---
+++ MyDay/View/AddTaskView.swift
@@ -0,0 +1,18 @@
+//
+//  AddTaskView.swift
+//  MyDay
+//
+
+import SwiftUI
+
+struct AddTaskView: View {
+    
+    
+    var body: some View {
+        Text("New task")
+    }
+}
+
+#Preview {
+    AddTaskView()
+}
